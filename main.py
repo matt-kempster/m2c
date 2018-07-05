894f564f8c99823ec733ca6ded3070d0e56b90fc
Matt Kempster <kidpixel@gmail.com>
2018-07-04 20:45:22 -0700

    Improve string use and documentation

--- main.py
+++ main.py
@@ -7,38 +7,28 @@ import sys
 import typing
 from typing import List, Union, Iterator, Optional
 
+
 @attr.s(frozen=True)
 class Register(object):
     register_name: str = attr.ib()
 
     def __str__(self):
-        return '$%s' % (self.register_name,)
+        return f'${self.register_name}'
 
 @attr.s(frozen=True)
 class GlobalSymbol(object):
     symbol_name: str = attr.ib()
 
     def __str__(self):
-        return '%s' % (self.symbol_name,)
+        return self.symbol_name
 
 @attr.s(frozen=True)
 class Macro(object):
     macro_name: str = attr.ib()
-    argument = attr.ib()
+    argument: 'Argument' = attr.ib()  # forward-declare
 
     def __str__(self):
-        return '%%%s(%s)' % (self.macro_name, self.argument)
-
-@attr.s(frozen=True)
-class AddressMode(object):
-    lhs = attr.ib()
-    rhs = attr.ib()
-
-    def __str__(self):
-        if self.lhs is not None:
-            return '%s(%s)' % (self.lhs, self.rhs)
-        else:
-            return '(%s)' % (self.rhs,)
+        return f'%{self.macro_name}({self.argument})'
 
 @attr.s(frozen=True)
 class NumberLiteral(object):
@@ -48,20 +38,31 @@ class NumberLiteral(object):
         return hex(self.value)
 
 @attr.s(frozen=True)
-class BinOp(object):
-    op = attr.ib()
-    lhs = attr.ib()
-    rhs = attr.ib()
+class AddressMode(object):
+    lhs: Optional[NumberLiteral] = attr.ib()
+    rhs: 'Argument' = attr.ib()
 
     def __str__(self):
-        return '%s %s %s' % (self.lhs, self.op, self.rhs)
+        if self.lhs is not None:
+            return f'{self.lhs}({self.rhs})'
+        else:
+            return f'({self.rhs})'
+
+@attr.s(frozen=True)
+class BinOp(object):
+    op: str = attr.ib()
+    lhs: 'Argument' = attr.ib()
+    rhs: 'Argument' = attr.ib()
+
+    def __str__(self):
+        return f'{self.lhs} {self.op} {self.rhs}'
 
 @attr.s(frozen=True)
 class JumpTarget(object):
     target: str = attr.ib()
 
     def __str__(self):
-        return '.%s' % (self.target,)
+        return f'.{self.target}'
 
 Argument = Union[
     Register,
@@ -86,7 +87,7 @@ def parse_number(elems: List[str]) -> int:
     number_str = parse_word(elems, valid_number)
     return ast.literal_eval(number_str)
 
-# Hacky parser.
+# Main parser.
 def parse_arg_elems(arg_elems: List[str]) -> Optional[Argument]:
     value: Optional[Argument] = None
 
@@ -119,7 +120,9 @@ def parse_arg_elems(arg_elems: List[str]) -> Optional[Argument]:
             macro_name = parse_word(arg_elems)
             assert macro_name in ('hi', 'lo')
             expect('(')
+            # Get the argument of the macro (which must exist).
             m = parse_arg_elems(arg_elems)
+            assert m is not None
             expect(')')
             return Macro(macro_name, m)
         elif tok == ')':
@@ -133,7 +136,9 @@ def parse_arg_elems(arg_elems: List[str]) -> Optional[Argument]:
             # There was possibly an offset, so value could be a NumberLiteral.
             assert value is None or isinstance(value, NumberLiteral)
             expect('(')
+            # Get what is being dereferenced.
             rhs = parse_arg_elems(arg_elems)
+            assert rhs is not None
             expect(')')
             value = AddressMode(value, rhs)
         elif tok in valid_word:
@@ -142,7 +147,7 @@ def parse_arg_elems(arg_elems: List[str]) -> Optional[Argument]:
             value = GlobalSymbol(parse_word(arg_elems))
         elif tok in '>+&':
             # Binary operators, used e.g. to modify global symbols or constants.
-            assert isinstance(value, NumberLiteral) or isinstance(value, GlobalSymbol)
+            assert isinstance(value, (NumberLiteral, GlobalSymbol))
 
             if tok == '>':
                 expect('>')
@@ -166,6 +171,7 @@ def parse_arg(arg: str) -> Optional[Argument]:
     arg_elems: List[str] = list(arg)
     return parse_arg_elems(arg_elems)
 
+
 @attr.s(frozen=True)
 class Instruction(object):
     mnemonic: str = attr.ib()
@@ -175,7 +181,7 @@ class Instruction(object):
         return self.mnemonic in ['b', 'beq', 'bne', 'bgez', 'bgtz', 'blez', 'bltz']
 
     def __str__(self):
-        return '    %s %s' % (self.mnemonic, ', '.join(str(arg) for arg in self.args))
+        return f'    {self.mnemonic} {", ".join(str(arg) for arg in self.args)}'
 
 def parse_instruction(line: str) -> Instruction:
     # First token is instruction name, rest is args.
@@ -187,12 +193,13 @@ def parse_instruction(line: str) -> Instruction:
         [parse_arg(arg_str.strip()) for arg_str in args_str.split(',')]))
     return Instruction(mnemonic, args)
 
+
 @attr.s(frozen=True)
 class Label(object):
     name: str = attr.ib()
 
     def __str__(self):
-        return '  .%s:' % (self.name, )
+        return f'  .{self.name}:'
 
 @attr.s
 class Function(object):
@@ -206,7 +213,8 @@ class Function(object):
         self.body.append(instruction)
 
     def __str__(self):
-        return 'glabel %s\n%s' % (self.name, '\n'.join(str(item) for item in self.body))
+        body = "\n".join(str(item) for item in self.body)
+        return f'glabel {self.name}\n{body}'
 
 @attr.s
 class Program(object):
@@ -226,7 +234,8 @@ class Program(object):
         self.current_function.new_label(label_name)
 
     def __str__(self):
-        return '# %s\n%s' % (self.filename, '\n\n'.join(str(function) for function in self.functions))
+        functions_str = '\n\n'.join(str(function) for function in self.functions)
+        return f'# {self.filename}\n{functions_str}'
 
 @attr.s(frozen=True)
 class Block(object):
@@ -236,10 +245,11 @@ class Block(object):
 
     def __str__(self):
         if self.label:
-            name = '%s (%s)' % (self.index, self.label.name)
+            name = f'{self.index} ({self.label.name})'
         else:
             name = self.index
-        return '# %s\n%s\n' % (name, '\n'.join(str(instruction) for instruction in self.instructions))
+        inst_str = '\n'.join(str(instruction) for instruction in self.instructions)
+        return f'# {name}\n{inst_str}\n'
 
 # Forward-declare the Node types since they're not defined yet.
 def is_loop_edge(node: 'Node', edge: 'Node'):
@@ -255,14 +265,17 @@ class BasicNode(object):
         return is_loop_edge(self, self.exit_edge)
 
     def __str__(self):
-        return '%s\n# %d -> %d%s' % (self.block, self.block.index, self.exit_edge.block.index, ' (loop)' if self.is_loop() else '')
+        return ''.join([
+            f'{self.block}\n',
+            f'# {self.block.index} -> {self.exit_edge.block.index}',
+            " (loop)" if self.is_loop() else ""])
 
 @attr.s(frozen=True)
 class ExitNode(object):
     block: Block = attr.ib()
 
     def __str__(self):
-        return '%s\n# %d -> ret' % (self.block, self.block.index)
+        return f'{self.block}\n# {self.block.index} -> ret'
 
 @attr.s(frozen=True)
 class ConditionalNode(object):
@@ -274,7 +287,13 @@ class ConditionalNode(object):
         return is_loop_edge(self, self.conditional_edge)
 
     def __str__(self):
-        return '%s\n# %d -> cond: %d%s, def: %s' % (self.block, self.block.index, self.conditional_edge.block.index, ' (loop)' if self.is_loop() else '', self.fallthrough_edge.block.index)
+        return ''.join([
+            f'{self.block}\n',
+            f'# {self.block.index} -> ',
+            f'cond: {self.conditional_edge.block.index}',
+            ' (loop)' if self.is_loop() else '',
+            ', ',
+            f'def: {self.fallthrough_edge.block.index}'])
 
 Node = Union[
     BasicNode,
@@ -282,13 +301,14 @@ Node = Union[
     ConditionalNode
 ]
 
+
 @attr.s(frozen=True)
 class FlowAnalysis(object):
     nodes: List[Node] = attr.ib()
 
 
 def do_flow_analysis(function: Function):
-    # Build blocks.
+    # First build blocks...
     blocks: List[Block] = []
 
     curr_index: int = 0
@@ -327,7 +347,7 @@ def do_flow_analysis(function: Function):
 
     # Now build edges.
 
-    # TODO: Is this necessary?
+    # Build the ExitNode first to avoid a special case later.
     exit_block: Block = blocks[-1]
     exit_node: ExitNode = ExitNode(exit_block)
     nodes: List[Node] = [exit_node]
@@ -397,6 +417,7 @@ def do_flow_analysis(function: Function):
     nodes.sort(key=lambda node: node.block.index)
     return FlowAnalysis(nodes)
 
+
 def decompile(filename: str, f: typing.TextIO) -> None:
     program: Program = Program(filename)
 
@@ -431,9 +452,11 @@ def decompile(filename: str, f: typing.TextIO) -> None:
     for node in flow_analysis.nodes:
         print(node)
 
+
 def main(filename: str) -> None:
     with open(filename, 'r') as f:
         decompile(filename, f)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or sys.argv[1] in ['-h, --help']:
