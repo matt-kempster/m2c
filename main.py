f69ec6abb1e0507554cada2065ea09f6942e2d61
Matt Kempster <kidpixel@gmail.com>
2018-07-06 01:49:03 -0700

    Add untested partial MIPS-to-AST converter
    I am only pushing this because I am going on vacation right now
    and figure someone may want to look at my partial progress before
    I get back. This code is liable to completely change later on.

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import re
 import string
 import sys
 
+from contextlib import contextmanager
+
 import typing
-from typing import List, Union, Iterator, Optional
+from typing import List, Union, Iterator, Optional, Dict, Callable, Any
+
+from pycparser import c_ast as c
 
 
 @attr.s(frozen=True)
 class Register(object):
     register_name: str = attr.ib()
+
+    def is_callee_save(self):
+        return re.match('s[0-7]', self.register_name)
 
     def __str__(self):
         return f'${self.register_name}'
@@ -418,7 +425,7 @@ def do_flow_analysis(function: Function):
     return FlowAnalysis(nodes)
 
 
-def decompile(filename: str, f: typing.TextIO) -> None:
+def decompile(filename: str, f: typing.TextIO) -> Program:
     program: Program = Program(filename)
 
     for line in f:
@@ -448,15 +455,299 @@ def decompile(filename: str, f: typing.TextIO) -> None:
     print(program.functions[1])
 
     print("\n\n### FLOW ANALYSIS")
-    flow_analysis = do_flow_analysis(program.functions[1])
+    flow_analysis: FlowAnalysis = do_flow_analysis(program.functions[1])
     for node in flow_analysis.nodes:
         print(node)
+
+    return program
+
+@attr.s
+class Store:
+    size: int = attr.ib()
+    source: Any = attr.ib()
+    dest: Any = attr.ib()
+    float: bool = attr.ib(default=False)
+    # TODO: Figure out
+
+@attr.s
+class TypeHint:
+    type: str = attr.ib()
+    value: Any = attr.ib()
+    # TODO: Figure out
+
+def load_upper(args):
+    if isinstance(args[1], Macro):
+        # Something like "lui REG %hi(arg)". Just take "arg".
+        assert args[1].macro_name == 'hi'
+        return args[1].argument
+    elif isinstance(args[1], BinOp):
+        # Something like "lui REG (lhs >> 16)". Just take "lhs".
+        assert args[1].op == '>>'
+        assert args[1].rhs == NumberLiteral(16)
+        return args[1].lhs
+    else:
+        # Something like "lui 0x1", meaning 0x10000. Shift left and return.
+        assert isinstance(args[1], NumberLiteral)
+        return c.BinaryOp(left=args[1], op='<<', right=NumberLiteral(16))
+
+def load_lower(args):
+    if isinstance(args[1], Macro):
+        # Something like "lw REG %lo(arg)(OFFSET)". We (hopefully) already
+        # handled this above, so return None.
+        assert args[1].macro_name == 'lo'
+        return None
+    else:
+        # Regular lw.
+        return TypeHint(type='s32', value=args[1])
+
+def handle_ori(args):
+    if isinstance(args[1], BinOp):
+        # Something like "ori REG (lhs & 0xFFFF)". We (hopefully) already
+        # handled this above, so return None.
+        assert args[1].op == '&'
+        assert args[1].rhs == NumberLiteral(0xFFFF)
+        return None
+    else:
+        # Regular bitwise OR.
+        return c.BinaryOp(left=reg[args[0]], op='<', right=reg[args[1]]),
+
+# BEWARE: This function (and its constituent inner functions) are currently
+# completely untested. I just pushed them to this branch because I am about
+# to go on vacation and figure someone might want to look at it.
+def translate_to_ast(function: Function):
+    # Initialize info about the function.
+    allocated_stack_size: int = 0
+    is_leaf: bool = True
+
+    local_vars_region_bottom: int = 0
+    return_addr_location: Optional[int] = None
+    callee_save_regs: Dict[str, int] = {}
+
+    flow_analysis: FlowAnalysis = do_flow_analysis(function)
+
+    def find_stack_info():
+        start_node: Node = flow_analysis.nodes[0]
+        for inst in start_node.block.instructions:
+            if inst.mnemonic == 'addiu' and inst.args[0].register_name == 'sp':
+                # Moving the stack pointer.
+                assert isinstance(inst.args[2], NumberLiteral)
+                allocated_stack_size = -inst.args[2].value
+            elif inst.mnemonic == 'sw' and inst.args[0].register_name == 'ra':
+                # Saving the return address on the stack.
+                assert isinstance(inst.args[2], AddressMode)
+                assert isinstance(inst.args[2].rhs, Register)
+                assert inst.args[2].rhs.register_name == 'sp'
+                is_leaf = False
+                if inst.args[2].lhs:
+                    return_addr_location = inst.args[2].lhs.value
+                else:
+                    # Note that this should only happen in the rare case that
+                    # this function only calls subroutines with no arguments.
+                    return_addr_location = 0
+            elif all(
+                inst.mnemonic == 'sw',
+                inst.args[0].is_callee_save(),
+                isinstance(inst.args[2], AddressMode),
+                inst.args[2].rhs.register_name == 'sp'
+            ):
+                # Initial saving of callee-save register onto the stack.
+                assert isinstance(inst.args[2].rhs, Register)
+                callee_save_regs[inst.args[0].register_name] = inst.args[2].lhs.value
+        if is_leaf and callee_save_regs:
+            local_vars_region_bottom = max(callee_save_regs.values()) + 4
+        elif is_leaf:
+            local_vars_region_bottom = 0
+        else:
+            local_vars_region_bottom = return_addr_location + 4
+
+    def in_local_var_region(location: int) -> bool:
+        return local_vars_region_bottom <= location < allocated_stack_size
+
+    def translate_block_body(block: Block, reg: Dict[Register, Any]):
+        cases_source_first_expression = {
+            # Storage instructions
+            'sb': lambda a: Store(size=8, source=reg[a[0]], dest=reg[a[1]]),
+            'sh': lambda a: Store(size=16, source=reg[a[0]], dest=reg[a[1]]),
+            'sw': lambda a: Store(size=32, source=reg[a[0]], dest=reg[a[1]]),
+            # Floating point storage/conversion
+            'swc1': lambda a: Store(size=32, source=reg[a[0]], dest=reg[a[1]], float=True),
+            'sdc1': lambda a: Store(size=64, source=reg[a[0]], dest=reg[a[1]], float=True),
+        }
+        cases_source_first_register = {
+            # Floating point moving instruction... the black sheep. >:(
+            'mtc1': lambda a: c.Cast(to_type='f32', expr=reg[a[0]]),
+        }
+        cases_branches = {  # TODO! These are wrong.
+            # Branch instructions/pseudoinstructions
+            'beq': lambda a: c.BinaryOp(left=reg[a[0]], op='==', right=reg[a[1]]),
+            'bne': lambda a: c.BinaryOp(left=reg[a[0]], op='!=', right=reg[a[1]]),
+            'blez': lambda a: c.BinaryOp(left=reg[a[0]], op='<=', right=NumberLiteral(0)),
+            'bgtz': lambda a: c.BinaryOp(left=reg[a[0]], op='>', right=NumberLiteral(0)),
+            'bltz': lambda a: c.BinaryOp(left=reg[a[0]], op='<', right=NumberLiteral(0)),
+            'bgez': lambda a: c.BinaryOp(left=reg[a[0]], op='>=', right=NumberLiteral(0)),
+        }
+        cases_float_branches = {  # TODO! Absolutely incomplete.
+            # Floating-point branch instructions
+            'bc1t': lambda a: a,
+            'bc1f': lambda a: a,
+        }
+        cases_jumps = {
+            # Unconditional jumps
+            'jal': lambda a: a[0],  # not sure what arguments!
+            'jr': lambda a: c.Return()  # not sure what to return!
+        }
+        cases_float_comp = {
+            # Floating point comparisons
+            'c.eq.s': lambda a: c.BinaryOp(left=reg[a[0]], op='==', right=reg[a[1]]),
+            'c.le.s': lambda a: c.BinaryOp(left=reg[a[0]], op='<=', right=reg[a[1]]),
+            'c.lt.s': lambda a: c.BinaryOp(left=reg[a[0]], op='<', right=reg[a[1]]),
+        }
+        cases_special = {
+            # Handle these specially to get better debug output.
+            # These should be unspecial'd at some point by way of an initial
+            # pass-through, similar to the stack-info acquisition step.
+            'lui': load_upper,
+            'lw': load_lower,
+            'ori': handle_ori,
+        }
+        cases_destination_first = {
+            # Flag-setting instructions
+            'slt': lambda a: c.BinaryOp(left=reg[a[1]], op='<', right=reg[a[2]]),
+            'slti': lambda a: c.BinaryOp(left=reg[a[1]], op='<', right=a[2]),
+            # LRU (non-floating)
+            'addi': lambda a: c.BinaryOp(left=reg[a[1]], op='+', right=a[2]),
+            'addu': lambda a: c.BinaryOp(left=reg[a[1]], op='+', right=reg[a[2]]),
+            'multu': lambda a: c.BinaryOp(left=reg[a[1]], op='*', right=reg[a[2]]),
+            'subu': lambda a: c.BinaryOp(left=reg[a[1]], op='-', right=reg[a[2]]),
+            'div': lambda a: (c.BinaryOp(left=reg[a[1]], op='/', right=reg[a[2]]),  # hi
+                              c.BinaryOp(left=reg[a[1]], op='%', right=reg[a[2]])), # lo
+            'negu': lambda a: c.UnaryOp(op='-', expr=reg[a[1]]),
+            # Hi/lo register uses (used after division)
+            'mfhi': lambda a: reg[Register("hi")],
+            'mflo': lambda a: reg[Register("lo")],
+            # Floating point arithmetic
+            'div.s': lambda a: c.BinaryOp(left=reg[a[1]], op='/', right=reg[a[2]]),
+            # Floating point conversions
+            'cvt.d.s': lambda a: c.Cast(to_type='(f64)', expr=reg[a[1]]),
+            'cvt.s.d': lambda a: c.Cast(to_type='(f32)', expr=reg[a[1]]),
+            'cvt.w.d': lambda a: c.Cast(to_type='(s32)', expr=reg[a[1]]),
+            # Bit arithmetic
+            'andi': lambda a: c.BinaryOp(left=reg[a[1]], op='&', right=reg[a[2]]),
+            'xori': lambda a: c.BinaryOp(left=reg[a[1]], op='^', right=reg[a[2]]),
+            'sll': lambda a: c.BinaryOp(left=reg[a[1]], op='<<', right=reg[a[2]]),
+            'srl': lambda a: c.BinaryOp(left=reg[a[1]], op='>>', right=reg[a[2]]),
+            # Move pseudoinstruction
+            'move': lambda a: reg[a[1]],
+            # Floating point moving instructions
+            'mfc1': lambda a: reg[a[1]],
+            # Loading instructions
+            'li': lambda a: a[1],
+            'lb': lambda a: TypeHint(type='s8', value=a[1]),
+            'lh': lambda a: TypeHint(type='s16', value=a[1]),
+            'lbu': lambda a: TypeHint(type='u8', value=a[1]),
+            'lhu': lambda a: TypeHint(type='u16', value=a[1]),
+            'lwu': lambda a: TypeHint(type='u32', value=a[1]),
+            # Floating point loading instructions
+            'lwc1': lambda a: TypeHint(type='f32', value=a[1]),
+            'ldc1': lambda a: TypeHint(type='f64', value=a[1]),
+        }
+        cases_uniques = {
+            **cases_source_first_expression,
+            **cases_source_first_register,
+            **cases_branches,
+            **cases_float_branches,
+            **cases_jumps,
+            **cases_float_comp,
+            **cases_special,
+            **cases_destination_first,
+        }
+        cases_repeats = {
+            # Addition and division, unsigned vs. signed, doesn't matter (?)
+            'addiu': 'addi',
+            'divu': 'div',
+            # Single-precision float addition is the same as regular addition.
+            'add.s': 'addu',
+            'mul.s': 'multu',
+            'sub.s': 'subu',
+            # TODO: These are absolutely not the same as their below-listed
+            # counterparts. However, it is hard to tell how to deal with doubles.
+            'add.d': 'addu',
+            'div.d': 'div.s',
+            'mul.d': 'mulu',
+            'sub.d': 'subu',
+            # Casting (the above applies here too)
+            'cvt.d.w': 'cvt.d.s',
+            'cvt.s.w': 'cvt.s.d',
+            'cvt.w.s': 'cvt.w.d',
+            # Floating point comparisons (the above also applies)
+            'c.lt.d': 'c.lt.s',
+            'c.eq.d': 'c.eq.s',
+            'c.le.d': 'c.le.s',
+            # Right-shifting.
+            'sra': 'srl',
+            # Flag setting.
+            'sltiu': 'slti',
+            'sltu': 'slt',
+        }
+
+        to_store: List[Store] = []
+        for instr in block.instructions:
+            mnemonic = instr.mnemonic
+            if mnemonic in cases_repeats:
+                # Determine "true" mnemonic.
+                mnemonic = cases_repeats[mnemonic]
+
+            # Figure out what code to generate!
+            # TODO: This is a side-note for when I inevitably forget to do it,
+            # but $zero should not be overwritten by div or anything like that.
+
+            if mnemonic in cases_source_first_expression:
+                # Store a value in a permanent place.
+                to_store.append(cases_source_first_expression[mnemonic](instr.args))
+            elif mnemonic in cases_source_first_register:
+                # Just 'mtc1'. It's reversed, so we have to specially handle it.
+                assert isinstance(instr.args[1], Register)  # could also assert float register
+                reg[instr.args[1]] = cases_source_first_register[mnemonic](instr.args)
+            elif mnemonic in cases_branches:
+                # TODO: Don't give up here.
+                pass
+            elif mnemonic in cases_float_branches:
+                # TODO: Don't give up here.
+                pass
+            elif mnemonic in cases_jumps:
+                result = cases_jumps[mnemonic](instr.args)
+                if isinstance(result, c.Return):
+                    # Return from the function.
+                    assert mnemonic == 'jr'
+                    # TODO: Maybe assert ExitNode?
+                    # TODO: Figure out what to return. (Look through $v0 and $f0)
+                else:
+                    # Function call. Well, let's double-check:
+                    assert mnemonic == 'jal'
+                    # TODO: Handle arguments... c.FuncCall(result, ???)
+                    pass
+            elif mnemonic in cases_float_comp:
+                # TODO: Don't give up here. (Similar to branches.)
+                pass
+            elif mnemonic in cases_special:
+                # TODO: One big problem here is that I'm storing the value
+                # into the FIRST (intermediate) register, not the final register
+                # as I should be! Whoops. This would be fixable right here
+                # in this if-statement (and an extra local var and flag).
+                assert isinstance(instr.args[0], Register)
+                cases_special[mnemonic](instr.args)
+                pass
+            elif mnemonic in cases_destination_first:
+                # Hey, I can do this one! (Maybe.)
+                assert isinstance(instr.args[0], Register)
+                reg[instr.args[0]] = cases_destination_first[mnemonic](instr.args)
+        print(f"To store: {to_store}")
+        print(f"Final register states: {reg}")
 
 
 def main(filename: str) -> None:
     with open(filename, 'r') as f:
-        decompile(filename, f)
-
+        program: Program = decompile(filename, f)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or sys.argv[1] in ['-h, --help']:
