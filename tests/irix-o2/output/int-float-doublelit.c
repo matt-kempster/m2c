83c958510c6889c9f9b68c5ad320a7f6d5dbf0a5
Simon Lindholm <simon.lindholm10@gmail.com>
2018-09-17 12:08:23 +0200

    Extend int-float-doublelit test

--- tests/irix-o2/output/int-float-doublelit.c
+++ tests/irix-o2/output/int-float-doublelit.c
@@ -1,10 +1,10 @@
 f32 test(f32 f12, s32 a1, s32 a3) {
-    D_410120 = (s32) f12;
-    D_410124 = (f32) a1;
+    D_410140 = (s32) f12;
+    D_410144 = (f32) a1;
     if ((a3 + 3) < 0)
     {
         return;
-        // (possible return value: ((f32) (a3 + 3) + (f32) ((f64) a2 + 5.0)))
+        // (possible return value: ((f32) (a3 + 3) + (f32) ((f64) (f32) (f64) (f32) ((f64) a2 + 5.0) + D_400130)))
     }
-    // (possible return value: ((f32) (a3 + 3) + (f32) ((f64) a2 + 5.0)))
+    // (possible return value: ((f32) (a3 + 3) + (f32) ((f64) (f32) (f64) (f32) ((f64) a2 + 5.0) + D_400130)))
 }
--- tests/orig/int-float-doublelit.c
+++ tests/orig/int-float-doublelit.c
@@ -6,5 +6,8 @@ float test(float f1, int i1, float f2, unsigned i2) {
     globalf = (float)i1;
     i2 += 3;
     f2 += 5.0;
+    f2 += 0.0;
+    f2 += 0.0f;
+    f2 += 5.3;
     return (float)i2 + f2;
 }
