33482a778b784b7ea19050642d3013aef89f4487
Matt Kempster <kidpixel@gmail.com>
2018-10-07 15:02:25 -0700

    Add two more complex loop tests

---
+++ tests/irix-o2/output/loop_with_if.c
@@ -0,0 +1,18 @@
+s32 test(s32 arg0) {
+    if (arg0 > 0)
+    {
+        if (0 == 5)
+        {
+
+        }
+        else
+        {
+
+        }
+        if ((0 + 4) < arg0)
+        {
+            goto loop_5;
+        }
+    }
+    // (possible return value: 0)
+}
