adbf0e29eb13ab09dd81d5afd24ed3d04193b4e5
Simon Lindholm <simon.lindholm10@gmail.com>
2018-12-20 16:13:29 +0100

    Deal with some instances of delay slot labels

--- src/flow_graph.py
+++ src/flow_graph.py
@@ -298,33 +298,50 @@ def build_blocks(function: Function) -> List[Block]:
     block_builder = BlockBuilder()
 
     body_iter: Iterator[Union[Instruction, Label]] = iter(function.body)
-    for item in body_iter:
+    def process(item: Union[Instruction, Label]) -> None:
+        process_after: List[Union[Instruction, Label]] = []
         if isinstance(item, Label):
             # Split blocks at labels.
             block_builder.new_block()
             block_builder.set_label(item)
         elif isinstance(item, Instruction):
+            process_after = []
             if item.is_delay_slot_instruction():
                 next_item = next(body_iter)
                 if isinstance(next_item, Label):
-                    # We currently don't handle delay slot instructions that
-                    # are jump targets, which can occur in -O2-compiled code.
-                    # Emit a nice error message instead.
-                    msg = [
-                        f"Label {next_item.name} refers to a delay slot; this is currently not supported.",
-                        "Please modify the assembly to work around it (e.g. copy the instruction",
-                        "to all jump sources and move the label, or add a nop to the delay slot)."
-                    ]
-                    if next_item.name.startswith('Ltemp'):
-                        msg += [
-                            "",
-                            "This label was auto-generated as the target for a branch-likely",
-                            "instruction (e.g. beql); you can also try to turn that into a non-likely",
-                            "branch if that's equivalent in this context, i.e., if it's okay to",
-                            "execute its delay slot unconditionally."
+                    # Delay slot is a jump target, so we need the delay slot
+                    # instruction to be in two blocks at once... In most cases,
+                    # we can just duplicate it. (This happens from time to time
+                    # in -O2-compiled code.)
+                    label = next_item
+                    next_item = next(body_iter)
+
+                    assert isinstance(next_item, Instruction), "Cannot have two labels in a row"
+
+                    # (Best-effort check for whether the instruction can be
+                    # executed twice in a row.)
+                    r = next_item.args[0] if next_item.args else None
+                    if all(a != r for a in next_item.args[1:]):
+                        process_after.append(label)
+                        process_after.append(next_item)
+                    else:
+                        msg = [
+                            f"Label {label.name} refers to a delay slot; this is currently not supported.",
+                            "Please modify the assembly to work around it (e.g. copy the instruction",
+                            "to all jump sources and move the label, or add a nop to the delay slot)."
                         ]
-                    raise DecompFailure('\n'.join(msg))
-                assert isinstance(next_item, Instruction)
+                        if label.name.startswith('Ltemp'):
+                            msg += [
+                                "",
+                                "This label was auto-generated as the target for a branch-likely",
+                                "instruction (e.g. beql); you can also try to turn that into a non-likely",
+                                "branch if that's equivalent in this context, i.e., if it's okay to",
+                                "execute its delay slot unconditionally."
+                            ]
+                        raise DecompFailure('\n'.join(msg))
+
+                if next_item.is_delay_slot_instruction():
+                    raise DecompFailure(f"Two delay slot instructions in a row is not supported:\n{item}\n{next_item}")
 
                 if item.is_branch_likely_instruction():
                     raise DecompFailure("Not yet able to handle general branch-likely instruction:\n" \
@@ -349,6 +366,12 @@ def build_blocks(function: Function) -> List[Block]:
                     block_builder.new_block()
             else:
                 block_builder.add_instruction(item)
+
+        for item in process_after:
+            process(item)
+
+    for item in body_iter:
+        process(item)
 
     # Throw away whatever is past the last "jr $ra" and return what we have.
     return block_builder.get_blocks()
