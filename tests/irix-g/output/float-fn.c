2a2786c5fd232477b0b8673518d1684a501b9deb
Simon Lindholm <simon.lindholm10@gmail.com>
2019-01-25 22:42:11 +0100

    Add float-fn test

---
+++ tests/irix-g/output/float-fn.c
@@ -0,0 +1,6 @@
+f32 test(f32 arg0)
+{
+    arg0 = (bitwise f32) func_004000DC(arg0);
+    arg0 = (f32) (bitwise f32) func_004000F4((f64) arg0);
+    return arg0;
+}
---
+++ tests/irix-o2/output/float-fn.c
@@ -0,0 +1,4 @@
+f32 test(void)
+{
+    return (f32) (bitwise f32) func_004000C4((f64) (bitwise f32) func_004000BC());
+}
