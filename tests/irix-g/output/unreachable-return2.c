c687f67665fde1765bed08ba5d9836b0d97430b8
Simon Lindholm <simon.lindholm10@gmail.com>
2019-02-16 15:25:19 +0100

    Add unreachable-return2 test
    Was fixed in 5644b9caac6a4f2403dca3e6c5898458b70e7ba4 or
    e6c708801f0f366e67214f078f899af7383ba23a.
    
    Closes #30.

---
+++ tests/irix-g/output/unreachable-return2.c
@@ -0,0 +1,10 @@
+void test(void)
+{
+loop_0:
+    if (D_4100F0 != 2)
+    {
+        D_4100F0 = 1;
+        goto loop_0;
+    }
+    return;
+}
---
+++ tests/irix-o2/output/unreachable-return2.c
@@ -0,0 +1,10 @@
+s32 test(void)
+{
+loop_1:
+    if (2 != D_4100E0)
+    {
+        D_4100E0 = 1;
+        goto loop_1;
+    }
+    return 2;
+}
