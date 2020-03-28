32208213cf817e7206137819a686a9de43d055aa
Simon Lindholm <simon.lindholm10@gmail.com>
2020-03-28 01:17:04 +0100

    Add infrastructure for computing struct layouts

---
+++ src/c_types.py
@@ -0,0 +1,285 @@
+"""This file handles variable types, function signatures and struct layouts
+based on a C AST. Based on the pycparser library."""
+
+from collections import defaultdict
+from typing import Union, Dict, Set, List, Tuple
+import sys
+
+import attr
+from pycparser import c_ast as ca
+from pycparser.c_ast import ArrayDecl, TypeDecl, PtrDecl, FuncDecl, IdentifierType
+from pycparser.c_generator import CGenerator
+
+from .error import DecompFailure
+
+Type = Union[PtrDecl, ArrayDecl, TypeDecl, FuncDecl]
+SimpleType = Union[PtrDecl, TypeDecl]
+
+
+@attr.s
+class StructField:
+    type: Type = attr.ib()
+    name: str = attr.ib()
+
+
+@attr.s
+class Struct:
+    fields: Dict[int, List[StructField]] = attr.ib()
+    # TODO: bitfields
+    size: int = attr.ib()
+    align: int = attr.ib()
+
+
+@attr.s
+class Function:
+    ret_type: Type = attr.ib()
+    # TODO: parameters
+
+
+@attr.s
+class TypeMap:
+    typedefs: Dict[str, Type] = attr.ib(factory=dict)
+    var_types: Dict[str, Type] = attr.ib(factory=dict)
+    functions: Dict[str, Function] = attr.ib(factory=dict)
+    struct_defs: Dict[str, Struct] = attr.ib(factory=dict)
+
+
+def to_c(node: ca.Node) -> str:
+    return CGenerator().visit(node)
+
+
+def basic_type(name: str) -> TypeDecl:
+    idtype = IdentifierType(names=[name])
+    return TypeDecl(declname=None, quals=[], type=idtype)
+
+
+def pointer(type: Type) -> Type:
+    return PtrDecl(quals=[], type=type)
+
+
+def resolve_typedefs(type: Type, typemap: TypeMap) -> Type:
+    while (
+        isinstance(type, TypeDecl)
+        and isinstance(type.type, IdentifierType)
+        and len(type.type.names) == 1
+        and type.type.names[0] in typemap.typedefs
+    ):
+        type = typemap.typedefs[type.type.names[0]]
+    return type
+
+
+def pointer_decay(type: Type, typemap: TypeMap) -> SimpleType:
+    real_type = resolve_typedefs(type, typemap)
+    if isinstance(real_type, ArrayDecl):
+        return PtrDecl(quals=[], type=real_type.type)
+    if isinstance(real_type, FuncDecl):
+        return PtrDecl(quals=[], type=type)
+    if isinstance(real_type, TypeDecl) and isinstance(real_type.type, ca.Enum):
+        return basic_type("int")
+    assert not isinstance(
+        type, (ArrayDecl, FuncDecl)
+    ), "resolve_typedefs can't hide arrays/functions"
+    return type
+
+
+def deref_type(type: Type, typemap: TypeMap) -> Type:
+    type = resolve_typedefs(type, typemap)
+    assert isinstance(type, (ArrayDecl, PtrDecl)), "dereferencing non-pointer"
+    return type.type
+
+
+def parse_function(fn: FuncDecl) -> Function:
+    # TODO: include parameters
+    return Function(ret_type=fn.type)
+
+
+def parse_constant_int(expr: "ca.Expression") -> int:
+    if isinstance(expr, ca.Constant):
+        try:
+            return int(expr.value.rstrip("lLuU"), 0)
+        except ValueError:
+            raise DecompFailure(f"Failed to parse {to_c(expr)} as an int literal")
+    if isinstance(expr, ca.BinaryOp):
+        lhs = parse_constant_int(expr.left)
+        rhs = parse_constant_int(expr.right)
+        if expr.op == "+":
+            return lhs + rhs
+        if expr.op == "-":
+            return lhs - rhs
+        if expr.op == "*":
+            return lhs * rhs
+        if expr.op == "<<":
+            return lhs << rhs
+        if expr.op == ">>":
+            return lhs >> rhs
+    raise DecompFailure(
+        f"Failed to evaluate expression {to_c(expr)} at compile time; only simple arithmetic is supported for now"
+    )
+
+
+def parse_struct(struct: Union[ca.Struct, ca.Union], typemap: TypeMap) -> Struct:
+    is_union = isinstance(struct, ca.Union)
+    assert (
+        struct.decls is not None
+    ), "parse_struct is only called on structs with .decls"
+    assert struct.decls, "Empty structs are not valid C"
+
+    def parse_struct_member(type: Type, field_name: str) -> Tuple[int, int]:
+        type = resolve_typedefs(type, typemap)
+        if isinstance(type, PtrDecl):
+            return 4, 4
+        if isinstance(type, ArrayDecl):
+            if type.dim is None:
+                raise DecompFailure(f"Array field {field_name} must have a size")
+            dim = parse_constant_int(type.dim)
+            size, align = parse_struct_member(type.type, field_name)
+            return size * dim, align
+        assert not isinstance(type, FuncDecl), "Struct can not contain a function"
+        inner_type = type.type
+        if isinstance(inner_type, (ca.Struct, ca.Union)):
+            if inner_type.name is not None and inner_type.name in typemap.struct_defs:
+                substr = typemap.struct_defs[inner_type.name]
+                return substr.size, substr.align
+            if inner_type.decls is not None:
+                substr = parse_struct(inner_type, typemap)
+                if inner_type.name is not None:
+                    typemap.struct_defs[inner_type.name] = substr
+                return substr.size, substr.align
+            raise DecompFailure(
+                f"Field {field_name} is of undefined struct type {inner_type.name}"
+            )
+        if isinstance(inner_type, ca.Enum):
+            return 4, 4
+        # Otherwise it has to be of type IdentifierType
+        if "double" in inner_type.names:
+            return 8, 8
+        if "float" in inner_type.names:
+            return 4, 4
+        if "short" in inner_type.names:
+            return 2, 2
+        if "char" in inner_type.names:
+            return 1, 1
+        if inner_type.names.count("long") == 2:
+            return 8, 8
+        return 4, 4
+
+    fields: Dict[int, List[StructField]] = defaultdict(list)
+    union_size = 0
+    align = 1
+    offset = 0
+    bit_offset = 0
+    for decl in struct.decls:
+        if not isinstance(decl, ca.Decl):
+            continue
+        field_name = f"{struct.name}.{decl.name}"
+
+        if decl.bitsize is not None:
+            # A bitfield "type a : b;" has the following effects on struct layout:
+            # - align the struct as if it contained a 'type' field.
+            # - allocate the next 'b' bits of the struct, going from high bits to low
+            #   within each byte.
+            # - ensure that 'a' can be loaded using a single load of the size given by
+            #   'type' (lw/lh/lb, unsigned counterparts). If it straddles a 'type'
+            #   alignment boundary, skip all bits up to that boundary and then use the
+            #   next 'b' bits from there instead.
+            width = parse_constant_int(decl.bitsize)
+            ssize, salign = parse_struct_member(decl.type, field_name)
+            align = max(align, salign)
+            if width == 0:
+                continue
+            if ssize != salign:
+                raise DecompFailure(f"Bitfield {field_name} is not of primitive type")
+            if width > ssize * 8:
+                raise DecompFailure(f"Width of bitfield {field_name} exceeds its type")
+            if is_union:
+                union_size = max(union_size, ssize)
+            else:
+                if offset // ssize != (offset + (bit_offset + width - 1) // 8) // ssize:
+                    bit_offset = 0
+                    offset = (offset + ssize) & -ssize
+                bit_offset += width
+                offset += bit_offset // 8
+                bit_offset &= 7
+            continue
+
+        if not is_union and bit_offset != 0:
+            bit_offset = 0
+            offset += 1
+
+        if decl.name is not None:
+            ssize, salign = parse_struct_member(decl.type, field_name)
+            align = max(align, salign)
+            offset = (offset + salign - 1) & -salign
+            fields[offset].append(StructField(type=decl.type, name=decl.name))
+            if is_union:
+                union_size = max(union_size, ssize)
+            else:
+                offset += ssize
+        elif (
+            isinstance(decl.type, (ca.Struct, ca.Union)) and decl.type.decls is not None
+        ):
+            if decl.type.name is None:
+                # C extension: anonymous struct/union, whose members are flattened
+                substr = parse_struct(decl.type, typemap)
+                align = max(align, substr.align)
+                offset = (offset + substr.align - 1) & -substr.align
+                for off, sfields in substr.fields.items():
+                    for field in sfields:
+                        fields[offset + off].append(field)
+                if is_union:
+                    union_size = max(union_size, substr.size)
+                else:
+                    offset += substr.size
+            else:
+                # Struct defined within another. Silly but valid C.
+                substr = parse_struct(decl.type, typemap)
+                typemap.struct_defs[decl.type.name] = substr
+
+    if not is_union and bit_offset != 0:
+        bit_offset = 0
+        offset += 1
+
+    size = union_size if is_union else (offset + align - 1) & -align
+    return Struct(fields=fields, size=size, align=align)
+
+
+def build_typemap(ast: ca.FileAST) -> TypeMap:
+    ret = TypeMap()
+    for item in ast.ext:
+        if isinstance(item, ca.Typedef):
+            ret.typedefs[item.name] = item.type
+        if isinstance(item, ca.FuncDef):
+            assert item.decl.name is not None, "cannot define anonymous function"
+            assert isinstance(item.decl.type, FuncDecl)
+            ret.functions[item.decl.name] = parse_function(item.decl.type)
+        if isinstance(item, ca.Decl) and isinstance(item.type, FuncDecl):
+            assert item.name is not None, "cannot define anonymous function"
+            ret.functions[item.name] = parse_function(item.type)
+
+    defined_function_decls: Set[ca.Decl] = set()
+
+    class Visitor(ca.NodeVisitor):
+        def visit_Struct(self, struct: ca.Struct) -> None:
+            if struct.decls and struct.name is not None and struct.decls is not None:
+                ret.struct_defs[struct.name] = parse_struct(struct, ret)
+
+        def visit_Union(self, union: ca.Union) -> None:
+            if union.decls and union.name is not None and union.decls is not None:
+                ret.struct_defs[union.name] = parse_struct(union, ret)
+
+        def visit_Decl(self, decl: ca.Decl) -> None:
+            if decl.name is not None:
+                ret.var_types[decl.name] = decl.type
+            if not isinstance(decl.type, FuncDecl):
+                self.visit(decl.type)
+
+        def visit_Enum(self, enum: ca.Enum) -> None:
+            if enum.name is not None:
+                ret.typedefs[enum.name] = basic_type("int")
+
+        def visit_FuncDef(self, fn: ca.FuncDef) -> None:
+            if fn.decl.name is not None:
+                ret.var_types[fn.decl.name] = fn.decl.type
+
+    Visitor().visit(ast)
+    return ret
