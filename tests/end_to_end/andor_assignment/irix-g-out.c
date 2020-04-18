a6b337dd0b3b592b169eaab14c7627d6e726faf9
Matt Kempster <kidpixel@gmail.com>
2020-04-18 12:01:27 -0700

    Add test for bad and/or assignments

---
+++ tests/end_to_end/andor_assignment/irix-g-out.c
@@ -0,0 +1,19 @@
+s32 test(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+    s32 sp24;
+    s32 sp20;
+    s32 sp1C;
+
+    sp24 = arg0 + arg1;
+    sp20 = arg1 + arg2;
+    sp1C = 0;
+    if ((((sp24 != 0) || (sp20 != 0)) || (sp20 != 0)) || (arg3 != 0))
+    {
+        sp1C = 1;
+    }
+    else
+    {
+
+    }
+    return sp1C;
+}
---
+++ tests/end_to_end/andor_assignment/irix-o2-out.c
@@ -0,0 +1,16 @@
+s32 test(s32 arg0, s32 arg1, s32 arg2, s32 arg3)
+{
+    s32 sp1C;
+    s32 temp_a0;
+    s32 phi_v1;
+
+    if (((((arg0 + arg1) != 0) || (temp_a0 != 0)) || (func_00400090(temp_a0) != 0)) || (arg3 != 0))
+    {
+        phi_v1 = 1;
+    }
+    else
+    {
+
+    }
+    return phi_v1;
+}
