10bd02781e16db65b349f842ce45a0dac209c297
Matt Kempster <kidpixel@gmail.com>
2020-05-16 12:12:24 -0700

    Add mixed and/or test

---
+++ tests/end_to_end/andor_mixed/irix-g-out.c
@@ -0,0 +1,22 @@
+s32 test(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    s32 spC;
+    s32 sp8;
+    s32 sp4;
+
+    spC = arg0 + arg1;
+    sp8 = arg1 + arg2;
+    sp4 = 0;
+    if (spC == 0) {
+        if ((sp8 == 0) || ((arg0 * arg1) == 0)) {
+            if ((arg3 != 0) && (arg0 != 0)) {
+block_5:
+                sp4 = arg0 + arg3;
+            }
+        } else {
+            goto block_5;
+        }
+    } else {
+        goto block_5;
+    }
+    return sp4;
+}
---
+++ tests/end_to_end/andor_mixed/orig.c
@@ -0,0 +1,15 @@
+int test(int a, int b, int c, int d) {
+    int var1;
+    int var2;
+    int ret;
+
+    var1 = a + b;
+    var2 = b + c;
+
+    ret = 0;
+    if (var1 || (var2 && (a * b)) || (d && a)) {
+        ret = a + d;
+    }
+
+    return ret;
+}
