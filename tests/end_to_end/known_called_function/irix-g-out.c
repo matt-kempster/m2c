5238b937ad64a01835b5b4491c848edd5336648b
Simon Lindholm <simon.lindholm10@gmail.com>
2020-05-19 10:20:38 +0200

    Add test for functions with stack parameters

--- tests/end_to_end/known_called_function/irix-g-out.c
+++ tests/end_to_end/known_called_function/irix-g-out.c
@@ -1,8 +1,8 @@
-void test(s32 x, short *y, s32 z) {
+void test(s32 x, short *y, s32 z, char *r, int *arg5) {
     int *sp1C;
 
     sp1C = NULL;
 loop_1:
-    sp1C = foo(sp1C, y);
+    sp1C = foo(sp1C, y, arg5);
     goto loop_1;
 }
--- tests/end_to_end/known_called_function/irix-o2-out.c
+++ tests/end_to_end/known_called_function/irix-o2-out.c
@@ -1,8 +1,8 @@
-void test(s32 x, short *y, s32 z) {
+void test(s32 x, short *y, s32 z, char *r, int *arg5) {
     int *phi_s0;
 
     phi_s0 = NULL;
 loop_1:
-    phi_s0 = foo(phi_s0, y);
+    phi_s0 = foo(phi_s0, y, arg5);
     goto loop_1;
 }
