02d56c32c843ed46a67fb54652307316f29dcf6e
Simon Lindholm <simon.lindholm10@gmail.com>
2020-05-23 23:31:27 +0200

    Add test for loop with unchanging phi

---
+++ tests/end_to_end/loop-selfassign-phi/irix-o2-out.c
@@ -0,0 +1,12 @@
+void test(s32 arg0) {
+    s32 phi_s0;
+
+    phi_s0 = arg0;
+loop_1:
+    phi_s0 = phi_s0;
+    if (phi_s0 < 3) {
+        goto loop_1;
+    }
+    phi_s0 = func_004000F4(phi_s0);
+    goto loop_1;
+}
