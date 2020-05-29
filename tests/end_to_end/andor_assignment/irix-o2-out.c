b1e30a5637c84f6c1b21b43262d5df9770a773c5
Matt Kempster <kidpixel@gmail.com>
2020-05-28 20:22:09 -0700

    Merge and run tests

--- tests/end_to_end/andor_assignment/irix-o2-out.c
+++ tests/end_to_end/andor_assignment/irix-o2-out.c
@@ -2,11 +2,10 @@ s32 test(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
     s32 sp24;
     s32 sp20;
     s32 sp1C;
-    s32 temp_ret;
     s32 temp_t0;
-    s32 temp_t1;
     s32 temp_t4;
     s32 temp_t7;
+    s32 temp_v0;
     s32 temp_v1;
     s32 phi_t0;
     s32 phi_t1;
@@ -20,16 +19,16 @@ s32 test(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
     temp_t7 = arg1 + arg2;
     sp1C = temp_t7;
     phi_t1 = temp_t7;
-    if ((((temp_t0 != 0) || (phi_t1 = temp_t7, (temp_t7 != 0))) || (sp20 = temp_t0, temp_ret = func_00400090(temp_t7), temp_t1 = temp_ret, phi_t1 = temp_t1, (temp_ret != 0))) || (arg3 != 0)) {
+    if ((((temp_t0 != 0) || (phi_t1 = temp_t7, (temp_t7 != 0))) || (sp20 = temp_t0, temp_v0 = func_00400090(temp_t7), phi_t1 = temp_v0, (temp_v0 != 0))) || (arg3 != 0)) {
         phi_v1 = 1;
     } else {
         phi_t0 = temp_t0;
-        phi_t1 = temp_t1;
+        phi_t1 = temp_v0;
         phi_v1 = -2;
         phi_a2 = arg2;
         if (arg0 != 0) {
             phi_t0 = temp_t0;
-            phi_t1 = temp_t1;
+            phi_t1 = temp_v0;
             phi_v1 = -1;
             phi_a2 = arg2;
         }
