969678f225dccbf1770e74939a1e6853550aad81
Simon Lindholm <simon.lindholm10@gmail.com>
2020-06-12 19:57:48 +0200

    Avoid too large recursion limit

--- src/main.py
+++ src/main.py
@@ -229,8 +229,8 @@ def parse_flags(flags: List[str]) -> Options:
 
 def main() -> None:
     # Large functions can sometimes require a higher recursion limit than the
-    # CPython default.
-    sys.setrecursionlimit(10 * sys.getrecursionlimit())
+    # CPython default. Cap to INT_MAX to avoid an OverflowError, though.
+    sys.setrecursionlimit(min(2 ** 31 - 1, 10 * sys.getrecursionlimit()))
     options = parse_flags(sys.argv[1:])
     sys.exit(run(options))
 
