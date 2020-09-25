d4f7134e2a6324a8fb6cdd412e717f2dfc12e247
Simon Lindholm <simon.lindholm10@gmail.com>
2020-09-25 13:43:29 +0200

    Fix early return nodes with --no-ifs

--- tests/end_to_end/no-ifs-early-returns/irix-o2-out.c
+++ tests/end_to_end/no-ifs-early-returns/irix-o2-out.c
@@ -8,6 +8,10 @@ void test(void *arg0, void *arg1) {
     if (temp_v1 == 0xF) {
         goto block_4;
     }
+    return;
+block_3:
+    *arg1 = (s32) (*arg1 + temp_v1);
+    return;
 block_4:
     *arg1 = (s32) (*arg1 - temp_v1);
 }
