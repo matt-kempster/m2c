db9995fb40216d3e25fc8b48d08d5b4bd8ddf7e1
Simon Lindholm <simon.lindholm10@gmail.com>
2020-09-29 15:07:57 +0200

    Strip some unnecessary parentheses

--- tests/end_to_end/doubles/irix-o2-out.c
+++ tests/end_to_end/doubles/irix-o2-out.c
@@ -2,7 +2,7 @@ f64 test(f32 arg0, s32 arg2, f64 arg4) {
     f64 temp_f0;
 
     temp_f0 = (((f64) arg2 * (f64) arg0) + ((f64) arg0 / arg4)) - 7.0;
-    if (((temp_f0 < arg4) || (temp_f0 == arg4)) || (9.0 < temp_f0)) {
+    if ((temp_f0 < arg4) || (temp_f0 == arg4) || (9.0 < temp_f0)) {
 
     } else {
 
