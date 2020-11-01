bfef5712c3a9ff443309db94b5d2b1ed71895a03
Simon Lindholm <simon.lindholm10@gmail.com>
2020-11-01 22:29:31 +0100

    Add a slightly buggy switch test

---
+++ tests/end_to_end/switch-with-if/irix-o2-out.c
@@ -0,0 +1,38 @@
+void test(s32 arg0) {
+    u32 temp_t1;
+    u32 temp_t6;
+
+    temp_t6 = arg0 - 1;
+    if (temp_t6 < 6U) {
+        goto **(&jtbl_4001A0 + (temp_t6 * 4)); // switch 1
+    case 0: // switch 1
+        D_4101D0 = 1;
+        if (arg0 == 1) {
+            D_4101D0 = 2;
+            goto block_7;
+        case 1: // switch 1
+            if (arg0 == 1) {
+                D_4101D0 = 1;
+            } else {
+                D_4101D0 = 2;
+            }
+        }
+    }
+block_7:
+    temp_t1 = arg0 - 1;
+    if (temp_t1 < 6U) {
+        goto **(&jtbl_4001B8 + (temp_t1 * 4)); // switch 2
+    case 0: // switch 2
+        D_4101D0 = 1;
+        if (arg0 == 1) {
+            D_4101D0 = 2;
+            return;
+        case 1: // switch 2
+            if (arg0 == 1) {
+                D_4101D0 = 1;
+                return;
+            }
+            D_4101D0 = 2;
+        }
+    }
+}
