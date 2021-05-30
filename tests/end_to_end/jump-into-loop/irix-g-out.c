bd62fcb5996db3e31d8d68b6219fcd97e9d2c073
Zach Banks <zjbanks@gmail.com>
2021-05-29 21:40:30 -0400

    Simplify `x = x op y` --> `x op= y`

--- tests/end_to_end/jump-into-loop/irix-g-out.c
+++ tests/end_to_end/jump-into-loop/irix-g-out.c
@@ -2,10 +2,10 @@ s32 test(s32 arg0) {
     goto block_3;
 loop_2:
     func_0040010C(arg0);
-    arg0 = arg0 + 1;
+    arg0 += 1;
 block_3:
     func_0040010C(arg0);
-    arg0 = arg0 * 2;
+    arg0 *= 2;
     if (arg0 < 4) {
         goto loop_2;
     }
