f19d91224f88336e2180bfd6c13749b22bbadc75
Zach Banks <zjbanks@gmail.com>
2021-06-11 14:24:17 -0400

    Bless test output

--- tests/end_to_end/global_decls/irix-o2-out.c
+++ tests/end_to_end/global_decls/irix-o2-out.c
@@ -1,9 +1,9 @@
 MIPS2C_UNK extern_fn(struct A *); // extern
 MIPS2C_UNK static_fn(struct A *); // static
 extern f32 extern_float;
-struct A static_A; // const; extra bytes: 5
 struct A *static_A_ptr = &static_A;
 s32 static_int = 0;
+struct A static_A; // type too large by 15; const
 
 s32 test(void) {
     static_int *= 0x1C8;
