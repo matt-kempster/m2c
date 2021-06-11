b26ada2d3260189caf5c3470b6e5c247602ac6a7
Zach Banks <zjbanks@gmail.com>
2021-06-10 21:21:37 -0400

    Update test output

--- tests/end_to_end/global_decls/irix-o2-out.c
+++ tests/end_to_end/global_decls/irix-o2-out.c
@@ -3,7 +3,7 @@ MIPS2C_UNK static_fn(struct A *); // static
 extern f32 extern_float;
 struct A static_A; // const; extra bytes: 5
 struct A *static_A_ptr = &static_A;
-s32 static_int = 0x1234;
+s32 static_int = 0;
 
 s32 test(void) {
     static_int *= 0x1C8;
