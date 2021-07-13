e72a283012aa27045a8938094fbe9191b373df3f
Simon Lindholm <simon.lindholm10@gmail.com>
2021-07-13 13:40:14 +0200

    Add tests

---
+++ tests/end_to_end/return-detection/floats-out.c
@@ -0,0 +1,24 @@
+f32 bar(); // extern
+
+f32 test(void) {
+    f32 temp_f0;
+    f32 phi_f0;
+
+    temp_f0 = bar();
+    phi_f0 = temp_f0;
+    phi_f0 = temp_f0;
+    if (4 == 0) {
+        goto block_3;
+    }
+block_1:
+    if (1 == 0) {
+        goto block_3;
+    }
+    bar();
+    phi_f0 = 2.0f;
+block_3:
+    if ((1 < 1) < 0) {
+        goto block_1;
+    }
+    return phi_f0;
+}
