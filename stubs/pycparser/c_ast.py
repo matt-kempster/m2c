260cf4a26d76dfdf5e2195a781d7a0cc0f4c728d
Zach Banks <zjbanks@gmail.com>
2021-11-06 13:55:32 -0400

    Fix stubs for pycparser, use Alignas instead of Type

--- stubs/pycparser/c_ast.py
+++ stubs/pycparser/c_ast.py
@@ -76,6 +76,7 @@ AnyNode = Union_[
     Statement,
     Type,
     InnerType,
+    "Alignas",
     "FuncDef",
     "EllipsisParam",
     "Enumerator",
@@ -93,6 +94,9 @@ class NodeVisitor:
         ...
 
     def generic_visit(self, node: Node) -> None:
+        ...
+
+    def visit_Alignas(self, node: Alignas) -> None:
         ...
 
     def visit_ArrayDecl(self, node: ArrayDecl) -> None:
@@ -237,6 +241,18 @@ class NodeVisitor:
         ...
 
 
+class Alignas(Node):
+    alignment: Union_[Expression, Typename]
+    coord: Optional[Coord]
+
+    def __init__(
+        self,
+        alignment: Union_[Expression, Typename],
+        coord: Optional[Coord] = None,
+    ):
+        ...
+
+
 class ArrayDecl(Node):
     type: Type
     dim: Optional[Expression]
@@ -344,7 +360,7 @@ class Continue(Node):
 class Decl(Node):
     name: Optional[str]
     quals: List[str]  # e.g. const
-    align: List[Type]
+    align: List[Alignas]
     storage: List[str]  # e.g. register
     funcspec: List[str]  # e.g. inline
     type: Type
@@ -355,7 +371,7 @@ class Decl(Node):
         self,
         name: Optional[str],
         quals: List[str],
-        align: List[Type],
+        align: List[Alignas],
         storage: List[str],
         funcspec: List[str],
         type: Type,
@@ -642,13 +658,13 @@ class TypeDecl(Node):
     declname: Optional[str]
     quals: List[str]
     type: InnerType
-    align: List[Type]
+    align: List[Alignas]
 
     def __init__(
         self,
         declname: Optional[str],
         quals: List[str],
-        align: List[Type],
+        align: List[Alignas],
         type: InnerType,
         coord: Optional[Coord] = None,
     ):
@@ -675,14 +691,14 @@ class Typedef(Node):
 class Typename(Node):
     name: None
     quals: List[str]
-    align: List[Type]
+    align: List[Alignas]
     type: Type
 
     def __init__(
         self,
         name: None,
         quals: List[str],
-        align: List[Type],
+        align: List[Alignas],
         type: Type,
         coord: Optional[Coord] = None,
     ):
