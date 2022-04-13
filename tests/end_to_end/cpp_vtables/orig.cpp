9a8320e617fe3906b31947d070f7a671bab4d28d
Zach Banks <zjbanks@gmail.com>
2022-04-13 15:08:29 -0400

    Add C++ test with vtables

---
+++ tests/end_to_end/cpp_vtables/orig.cpp
@@ -0,0 +1,38 @@
+// This file needs to be compiled with the `-inline off` flag
+
+class Base {
+public:
+    int member;
+    virtual int virtual_method(int x);
+    static int static_method(int x) {
+        return x * 2;
+    }
+    int method(int x) {
+        return x * 3;
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived(int x) {
+        member = x;
+    }
+    int virtual_method(int x) {
+        return x * 4;
+    }
+};
+
+int compute(Derived &d, int x) {
+    d.member = x;
+    x = d.virtual_method(x);
+    x = d.static_method(x);
+    x = d.method(x);
+    return x;
+}
+
+extern "C" {
+    int test(int x) {
+        Derived d(x);
+        return compute(d, x);
+    }
+}
