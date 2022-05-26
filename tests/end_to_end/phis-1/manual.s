fbcaa4eed94e31c67ea6d8812896e0dd47283018
Simon Lindholm <simon.lindholm10@gmail.com>
2022-05-27 00:04:39 +0200

    Fix test reading $ra from nowhere

--- tests/end_to_end/phis-1/manual.s
+++ tests/end_to_end/phis-1/manual.s
@@ -2,15 +2,14 @@ glabel test
   addiu $sp, $sp, -0x110
   jal   foo
    nop
-  move  $s5, $v0
+  move  $t1, $v0
   lbu   $t0, ($zero)
   bnez  $t0, .L0043D650
    nop
-  sw    $s5, 0x10c($sp)
-  lw    $s5, 0x10c($sp)
+  sw    $t1, 0x10c($sp)
+  lw    $t1, 0x10c($sp)
 .L0043D650:
-  sb    $zero, 3($s5)
-  sb    $zero, 4($s5)
-  lw    $ra, 0x3c($sp)
+  sb    $zero, 3($t1)
+  sb    $zero, 4($t1)
   jr    $ra
    addiu $sp, $sp, 0x110
