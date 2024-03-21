e247f9cfefb3828decfdf8cdfbe29586fe93fe96
Angie <angheloalf95@gmail.com>
2024-03-21 14:07:27 -0300

    black

--- m2c/arch_mips.py
+++ m2c/arch_mips.py
@@ -961,7 +961,12 @@ class MipsArch(Arch):
                 if loc is None:
                     return []
                 elif size == 16:
-                    return [loc, replace(loc, offset=loc.offset + 4), replace(loc, offset=loc.offset + 8), replace(loc, offset=loc.offset + 12)] 
+                    return [
+                        loc,
+                        replace(loc, offset=loc.offset + 4),
+                        replace(loc, offset=loc.offset + 8),
+                        replace(loc, offset=loc.offset + 12),
+                    ]
                 elif size == 8:
                     return [loc, replace(loc, offset=loc.offset + 4)]
                 else:
