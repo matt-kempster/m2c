395b4da1fd9aada5539f5487736fd1e8ec709374
angie <angheloalf95@gmail.com>
2024-03-21 09:46:01 -0300

    Make test for paddub

---
+++ tests/end_to_end/ps2/manual_mwcc-out.c
@@ -0,0 +1,7 @@
+? func2__FiPPc(s32, ?);                             /* extern */
+? func__Fv();                                       /* extern */
+
+void test(s32 arg0, ? arg1) {
+    func__Fv();
+    func2__FiPPc(arg0, arg1);
+}
