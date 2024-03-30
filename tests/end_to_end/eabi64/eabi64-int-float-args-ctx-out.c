1b7f5ea3cb881964f62c524030011e86184975a2
Angie <angheloalf95@gmail.com>
2024-03-30 09:23:32 -0300

    i'm dumb

--- tests/end_to_end/eabi64/eabi64-int-float-args-ctx-out.c
+++ tests/end_to_end/eabi64/eabi64-int-float-args-ctx-out.c
@@ -1,7 +1,4 @@
-? func_with_args(?, ?, ?);                          /* extern */
-? no_args_func();                                   /* extern */
-
 void test(void) {
     no_args_func();
-    func_with_args(0x40000000, 1, 2);
+    func_with_args(2.0f, M2C_ERROR(/* Read from unset register $f14 */), M2C_ERROR(/* Read from unset register $a2 */), M2C_ERROR(/* Read from unset register $a3 */));
 }
