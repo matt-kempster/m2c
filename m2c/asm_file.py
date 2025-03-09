4291523475c6dcb05f8fdf5e7d94380686a16366
Simon Lindholm <simon.lindholm10@gmail.com>
2025-03-09 10:48:17 +0100

    Cope with non-local labels

--- m2c/asm_file.py
+++ m2c/asm_file.py
@@ -337,7 +337,7 @@ def parse_file(f: typing.TextIO, arch: ArchAsm, options: Options) -> AsmFile:
 
     re_whitespace_or_string = re.compile(r'\s+|"(?:\\.|[^\\"])*"')
     re_local_glabel = re.compile("L(_.*_)?[0-9A-F]{8}")
-    re_local_label = re.compile("loc_|locret_|def_|lbl_|LAB_|jump_")
+    re_local_label = re.compile("loc_|locret_|def_|lbl_|LAB_|jump_|_[0-9A-F]{8}$")
     re_label = re.compile(r'(?:([a-zA-Z0-9_.$]+)|"([a-zA-Z0-9_.$<>@,-]+)"):')
 
     T = TypeVar("T")
