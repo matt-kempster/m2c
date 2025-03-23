90f487f0d0b6ff374c4d403087a9ed116e262290
dbalatoni13 <stubiflex.junior@gmail.com>
2025-03-23 07:27:55 +0100

    Add a <= b pattern for MWCC 2.6

--- tests/end_to_end/comparison2/mwcc-o4p-out.c
+++ tests/end_to_end/comparison2/mwcc-o4p-out.c
@@ -4,9 +4,9 @@ void test(u32 arg0, s32 arg1, s32 arg2) {
     global = arg1 == arg0;
     global = arg0 != arg2;
     global = arg0 < arg1;
-    global = (arg1 >> 0x1F) + (arg0 >> 0x1FU) + M2C_CARRY;
+    global = arg0 <= arg1;
     global = arg1 < arg0;
-    global = ((s32) arg0 >> 0x1F) + ((u32) arg1 >> 0x1FU) + M2C_CARRY;
+    global = (u32) arg1 <= (s32) arg0;
     global = arg0 == 0;
     global = arg1 != 0;
 }
