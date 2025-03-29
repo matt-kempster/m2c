5c29967dfcec794758898852a232eb44b4464215
dbalatoni13 <stubiflex.junior@gmail.com>
2025-03-29 23:21:30 +0100

    Revert "Add partial context to struct-copy2"
    This reverts commit bb4cb494714d61725c626ca292da9ae2da1d66e3.

--- tests/end_to_end/struct-copy2/mwcc-o0p-out.c
+++ tests/end_to_end/struct-copy2/mwcc-o0p-out.c
@@ -1,5 +1,5 @@
-static ? s7;
 static ? d7;
+static ? s7;
 
 void test(void) {
 
@@ -33,7 +33,7 @@ void test_6(? *arg0, ? *arg1) {
         *arg0 = *arg1;                                  /* size 0xE */
 }
 
-void test_7(Test7 *a, Test7 *b) {
-        *a = s7;                                        /* size 0xF */
-        d7 = *b;                                        /* size 0xF */
+void test_7(s32 arg0, s32 arg1) {
+    M2C_STRUCT_COPY(arg0, arg1, 0xF);
+    M2C_STRUCT_COPY(&d7, &s7, 0xF);
 }
